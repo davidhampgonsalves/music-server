ffefa16d4856e000476717d9d0cd2e6d40e92499
david hamp-gonsalves <davidhampgonsalves@gmail.com>
2023-04-21 23:36:42 -0300

    update remove to new path

--- remove-starred-tracks.sh
+++ remove-starred-tracks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sqlite3 /var/lib/navidrome/navidrome.db "select path from annotation a, media_file f where a.item_id = f.id AND (a.starred OR a.rating = 1);" | xargs -d '\n' rm
+sqlite3 /data/data/com.termux/files/home/navidrome/data/navidrome.db "select path from annotation a, media_file f where a.item_id = f.id AND (a.starred OR a.rating = 1);" | xargs -d '\n' rm
