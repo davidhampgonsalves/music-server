51d7223f18840480aa8fead243ed594905662d73
david hamp-gonsalves <dhamp-gonsalves@gitlab.com>
2024-12-29 17:05:35 -0400

    update scripts

--- bin/add-folder.sh
+++ bin/add-folder.sh
@@ -3,6 +3,6 @@ mkdir -p /tmp/music-to-transfer
 
 find -E . -regex '.*\.(mp3|flac|wav)'  -exec bash -c 'F=$(cat /dev/urandom | gtr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1); ffmpeg -i "{}" -codec:a libmp3lame -qscale:a 6 "/tmp/music-to-transfer/$F.mp3"' \;
 
-mv /tmp/music-to-transfer/* /Volumes/sharing/music
+mv /tmp/music-to-transfer/* /Volumes/share/music
 
 rm -rf /tmp/music-to-transfer
--- remove-starred-tracks.sh
+++ remove-starred-tracks.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# delete songs with 1 star rating or that are hearted
 sqlite3 /home/david/navidrome/data/navidrome.db "select path from annotation a, media_file f where a.item_id = f.id AND (a.starred OR a.rating = 1);" | xargs -d '\n' rm
+
+# delete albums with 1 star rating or that are hearted
 sqlite3 /home/david/navidrome/data/navidrome.db "select path from annotation a, media_file f, album al where a.item_id = al.id AND f.album_id = al.id AND (a.starred OR a.rating = 1);" | xargs -d '\n' rm
+
+# delete duplicate tracks
+sqlite3 /home/david/navidrome/data/navidrome.db "SELECT path FROM media_file WHERE rowid NOT IN (SELECT MIN(rowid) FROM media_file GROUP BY artist_id, album_id, title, size);" | xargs -d '\n' rm
+
+
